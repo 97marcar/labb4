01da57c495e53a0ffa0d3e7793e65dac563af673
97marcar <marcus.carlsson97@gmail.com>
2017-02-21 16:00:36 +0100

    sista

--- labb4/src/lab4/GomokuMain.java
+++ labb4/src/lab4/GomokuMain.java
@@ -7,11 +7,17 @@ import lab4.gui.GomokuGUI;
 public class GomokuMain {
 
 	public static void main(String[] args) {
-		GomokuClient client = new GomokuClient(4000);
+		GomokuClient client = new GomokuClient(4003);
+		GomokuClient client2 = new GomokuClient(4004);
+		
 		GomokuGameState gameState = new GomokuGameState(client);
+		GomokuGameState gameState2 = new GomokuGameState(client2);
+		
 		GomokuGUI gui = new GomokuGUI(gameState, client);
+		GomokuGUI gui2 = new GomokuGUI(gameState2, client2);
 
 
 	}
 
 }
+
